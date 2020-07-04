171048acc8f5b82c08f4e9327c6aa4b0ac85ca56
Maireen Espiritu <imaireen@gmail.com>
2020-07-03 20:50:34 -0700

    installed bcrypt and configured

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost/nodeauth', {useUnifiedTopology: true, useNewUrlParser: true}); // url of database
 var db = mongoose.connection; // variable holds connection to the database
+var bcrypt = require('bcryptjs');
 
 // user schea
 var UserSchema = mongoose.Schema({
@@ -27,5 +28,11 @@ var User = module.exports = mongoose.model('User', UserSchema);
 
 // callbacks are functions that can be passed into another function
 module.exports.createUser = function(newUser, callback){
-    newUser.save(callback);
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            // Store hash in your password DB.
+            newUser.password = hash; // hashes password before it gets saved
+            newUser.save(callback);
+        });
+    });
 }
